2b7b5a3eaefb2664c896f221c21d71ebe4ddfdb5
kandksoftware <57774027+kandksoftware@users.noreply.github.com>
2021-06-15 14:01:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
+(function(){
+  'use strict'
 
+  const $ = id => document.getElementById(id)
+  
+  function XBuilder(){
+    this.data = [{
+      image:'macro-terminal-icon',
+      name:'Macro Terminal CNC',
+      desc:' - Learn, test, and simulate better with Macro Terminal CNC',
+      link:'https://kandksoftware.github.io/macro-terminal-pro/'
+    },{
+      image:'macro-mill-icon',
+      name:'Macro Mill Plus CNC',
+      desc:' - powerful 3D simulator',
+      link:'https://kandksoftware.github.io/macro-terminal-pro/'
+    },{
+      image:'macro-lathe-icon',
+      name:'Macro Lathe CNC',
+      desc:' - simulator for the CNC lathes',
+      link:'https://kandksoftware.github.io/macro-terminal-pro/'
+    },{
+      image:'macro-converter-icon',
+      name:'Macro Converter CNC',
+      desc:' - convert CNC macro into a pure g-code',
+      link:'https://kandksoftware.github.io/macro-terminal-pro/'
+    },{
+      image:'macro-router-icon',
+      name:'Macro Router CNC',
+      desc:' - use a power of macro for CNC routers',
+      link:'https://kandksoftware.github.io/macro-terminal-pro/'
+    }]
+    
+    function template(o){
+      return `<a href="${ o.link }">
+              <div class="app-item app-margin-bottom-5px">
+                <div class="app-float app-icon-container">
+                  <img src="resources/${ o.image }.png" alt="icon image"/>
+                </div>
+                <div class="app-float app-description-container">
+                  <div class="app-description-center">
+                    ${ '<span class="bold">' + o.name + '</span>' + o.desc }
+                  </div>
+                </div>
+              </div>
+            </a>`
+    }
+
+    this.build = function(){
+      const app = $('app')
+      if(app !== null){
+        app.classList.remove('hide')
+      }
+      return `<h2>Select your software:</h2>` + this.data.map(d => template(d)).join('')
+    }
+  }
+
+  $('app-entry').innerHTML = new XBuilder().build()
+
+})()
