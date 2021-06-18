6f74b760cded1a19630a7d7223a01bb0c4eae4ea
kandksoftware <57774027+kandksoftware@users.noreply.github.com>
2021-06-19 01:36:32 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,49 @@
+(function(){
+  'use strict'
+    
+  var builditems = function(array){
+    var el = document.querySelector('.items')
+    el.classList.remove('hide')
+    el.innerHTML = array.map(a => {
+      return `<div class="items__item" onclick="location.href='${ a.link }';">
+              <img src="resources/${ a.img }.png" alt="${ a.name }"/>
+              <div class="items__desc">
+                <h3>${ a.name }</h3>
+                <h4>${ a.desc }</h4>
+              </div>
+            </div>`
+    }).join('')
+  }
+
+  builditems([{
+    name:'Macro Terminal CNC',
+    img:'macro-terminal-icon',
+    desc:'Learn, test, and simulate better with Macro Terminal CNC',
+    link:'https://kandksoftware.github.io/macro-terminal-pro/'
+  },{
+    name:'Macro Mill Plus CNC',
+    img:'macro-mill-icon',
+    desc:'powerful 3D simulator',
+    link:'https://kandksoftware.github.io/macro-mill-plus-cnc/'
+  },{
+    name:'Macro Lathe CNC',
+    img:'macro-lathe-icon',
+    desc:'simulator for the CNC lathes',
+    link:'https://kandksoftware.github.io/macro-lathe-cnc/'
+  },{
+    name:'Macro Converter CNC',
+    img:'macro-converter-icon',
+    desc:'convert CNC macro into a pure g-code',
+    link:'https://kandksoftware.github.io/macro-converter-cnc/'
+  },{
+    name:'Macro Router CNC',
+    img:'macro-router-icon',
+    desc:'use a power of macro for CNC routers',
+    link:'https://kandksoftware.github.io/macro-router-cnc/'
+  },{
+    name:'Mobile CAM CNC',
+    img:'mobile_cam_icon',
+    desc:'a fast CNC program generator',
+    link:'https://mobilecamcnc.github.io/products/'
+  }])
+})();
